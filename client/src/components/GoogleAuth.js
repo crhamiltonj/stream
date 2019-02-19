20a4ea43c7bc22fe98443ad4b6b72d647f4abd57
Charles Hamilton <crhamiltonj@gmail.com>
2019-02-19 11:30:48 -0500

    Added auth logic and login logout buttons.

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null }
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '242007007654-7op8bp1kkcvfqaqnqqc0smf16iirff6e.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState( { isSignedIn: this.auth.isSignedIn.get() });
+
+    };
+    
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (<button className="ui red google button">
+            <i className="google icon" />
+            Sign Out
+            </button>)
+        } else {
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign in with Google
+                </button>
+            );
+        }
+    }
+
+    render() {
+        return <div>{this.renderAuthButton()}</div>;
+    }
+};
+
+export default GoogleAuth;
